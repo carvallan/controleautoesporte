f373ee87795b49d437a01895912799b233e32915
carvallan <carvallan@gmail.com>
2018-06-24 20:44:02 -0400

    primeiro sinc

---
+++ services/db/conexao.php
@@ -0,0 +1,24 @@
+<?php
+    
+    try{
+        $pdo = new PDO("mysql:host=mysql472.umbler.com", "autoesporte", "autoesporte1", array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+                PDO::ATTR_PERSISTENT => false,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            )
+        );
+        
+        // Set the PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // Print host information
+         
+        // $pdo->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
+        $sql = "use dbautoesporte";
+        $pdo->exec($sql);
+       echo "Connect Successfully. Host info: " 
+    } catch(PDOException $e){
+        die("ERROR: Could not connect. " . $e->getMessage());
+    }
+?>
