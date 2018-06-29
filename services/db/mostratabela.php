f0e66294befbb1bc78dfc94d29973d531221bbe9
carvallan <carvallan@gmail.com>
2018-06-29 14:14:23 -0400

    att validacao do form

--- services/db/mostratabela.php
+++ services/db/mostratabela.php
@@ -14,10 +14,23 @@ $sql = mysqli_query($cx, "SELECT * FROM clientes") or die(
 while($aux = mysqli_fetch_assoc($sql))
 {
 
-  echo "<table>";
+  echo "<table class="table table-hover bg-light"/>";
+   echo "<thead/>"
+      echo "<tr>"
+        echo  "<th scope="col"/>"Modelo"</th/>"
+       echo "<th scope="col"/>"Marca"</th/>"
+       echo "<th scope="col"/>"Versão"</th/>"
+         echo "<th scope="col"/>"Ano"</th/>"
+       echo "<th scope="col"/>"Motor"</th/>"
+      echo  "<th scope="col"/>"Nome"</th/>"
+        echo "<th scope="col"/>"Telefone"</th/>"
+        echo "<th scope="col"/>"Detalhes da Negociação"</th/>"
+        echo "<th scope="col"/>"Valor pré negociado"</th/>"
+      echo "</tr/>"
+    echo "</thead/>"
     echo "<tbody />";
   echo "<tr />";
-      echo "<td>".$aux["nome"]."</td /><br />";
+      echo "<td >".$aux["nome"]."</td /><br />";
       echo "<td>".$aux["contato"]."</td /><br />";
       echo "<td>".$aux["redesocial"]."</td /><br />";
       echo "<td>".$aux["carronegociado"]."</td /><br />";
