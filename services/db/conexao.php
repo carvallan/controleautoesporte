c6f8aa1c76a837d9770cb6a44c82b819e7e7284d
carvallan <carvallan@gmail.com>
2018-07-03 16:36:31 -0400

    obtendo resultados do banco

--- services/db/conexao.php
+++ services/db/conexao.php
@@ -17,8 +17,8 @@
         // $pdo->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
         $sql = "use dbautoesporte";
         $pdo->exec($sql);
-        echo "Dado inserido com sucesso!"
-        header("Location: https://controleautoesporte-com-br.umbler.net/index.html"); ;
+        echo "Dado inserido com sucesso!";
+        header("Location: https://controleautoesporte-com-br.umbler.net/index.html"); 
     } catch(PDOException $e){
         die("ERROR: Could not connect. " . $e->getMessage());
     }
