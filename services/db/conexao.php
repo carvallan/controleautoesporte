c1723ebc151c8f2f97c296fbd41a06a8c8512e1b
carvallan <carvallan@gmail.com>
2018-07-03 16:42:50 -0400

    obtendo resultados do banco

--- services/db/conexao.php
+++ services/db/conexao.php
@@ -17,7 +17,8 @@
         // $pdo->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
         $sql = "use dbautoesporte";
         $pdo->exec($sql);
-       echo "Dado inserido com sucesso!";
+      header( 'refresh: 5; url = https://controleautoesporte-com-br.umbler.net/tabela.html' );
+echo 'Dado inserido com sucesso!';
     } catch(PDOException $e){
         die("ERROR: Could not connect. " . $e->getMessage());
     }
