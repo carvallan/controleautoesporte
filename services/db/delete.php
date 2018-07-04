d89dde8c4203a8c5e71a5e3386a7fee627dbcc49
carvallan <carvallan@gmail.com>
2018-07-04 15:30:33 -0400

    att da login

--- services/db/delete.php
+++ services/db/delete.php
@@ -2,10 +2,10 @@
 $id = 5; 
    
 try {
-  $pdo = new PDO('mysql:host=localhost;dbname=meuBancoDeDados', $username, $password);
+  $pdo = new PDO('mysql:host=mysql472.umbler.com;dbname=dbautoesporte', $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    
-  $stmt = $pdo->prepare('DELETE FROM minhaTabela WHERE id = :id');
+  $stmt = $pdo->prepare('DELETE FROM clientes WHERE id = :id');
   $stmt->bindParam(':id', $id); 
   $stmt->execute();
      
